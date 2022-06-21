d7fd20379873c74221c8a5a19773d6ca1b19884e
Henrique Almeida <93076807+rikeealmeida@users.noreply.github.com>
2022-06-21 10:00:46 -0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -57,9 +57,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "c9e54538ec18758953a1bfafc93d0377",
-"/": "c9e54538ec18758953a1bfafc93d0377",
-"main.dart.js": "b88ea58e0b2dd795aeb4c31d92b136dc",
+"index.html": "1bbc4fe742cc01eb2fc6715ba1299e48",
+"/": "1bbc4fe742cc01eb2fc6715ba1299e48",
+"main.dart.js": "668da4158684336e95567ebf7627f6de",
 "manifest.json": "19841c5827ccb9647a7cde86a7eaca08",
 "version.json": "2c8d06181daa3ea4025f698bb6dcad7e"
 };
